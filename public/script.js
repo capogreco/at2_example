22f8f7e3112308c71a58aff6c7c4759156394ad2
capogreco <thomas.capogreco@gmail.com>
2024-03-27 11:09:15 +1100

    animating a pink square

--- public/script.js
+++ public/script.js
@@ -2,21 +2,33 @@ document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
 const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+
+function resize_canvas () {
+   cnv.width = innerWidth
+   cnv.height = innerHeight
+}
+
+resize_canvas ()
+
+window.onresize = resize_canvas
 
 const ctx = cnv.getContext (`2d`)
 
+let x_pos = -100
+
 const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
+   ctx.fillStyle = `turquoise` // fill (`turquoise`)
    ctx.fillRect (0, 0, innerWidth, innerHeight)
+   
+   ctx.fillStyle = `hotpink`
+   ctx.fillRect (x_pos, innerHeight / 2 - 50, 100, 100)
+
+   x_pos += 1
+
+   if (x_pos > innerWidth) x_pos = -100
 
    requestAnimationFrame (draw_frame)
 }
 
 draw_frame ()
 
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
